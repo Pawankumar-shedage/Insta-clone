50097e16a54b42b04cfcec0ae8bab49497b846bc
Pawankumar Shedage <pawankumarshedage@gmail.com>
2024-05-21 17:20:57 +0530

    Breadcrumbs like btn

--- src/Components/Profile/ViewPostModal/ViewPostModal.jsx
+++ src/Components/Profile/ViewPostModal/ViewPostModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useAuth } from "../../../AuthContext/AuthProvider";
@@ -8,15 +9,19 @@ import { useFirebase } from "../../../FirebaseSetUp/FirebaseContext";
 // icons
 import { IoIosMore } from "react-icons/io";
 import { EmojiDrawer } from "../../Messages/EmojiDrawer";
+import { useProfilePhotoOfCurrUser } from "../ProfilePhotoContext/ProfilePhotoContext";
 
 // eslint-disable-next-line no-unused-vars
-export const ViewPostModal = ({ post }) => {
+export const ViewPostModal = ({ post: userPost }) => {
   const { currentUser } = useAuth();
-  const { getUserById, getProfilePhoto } = useFirebase();
+  const { getUserById, updateUserPostData } = useFirebase();
+  const { dpLoading, dpCurrUser } = useProfilePhotoOfCurrUser();
 
   const [user, setUser] = useState(null);
-
   const [openEmojiDrawer, setOpenEmojiDrawer] = useState(false);
+  const [post, setPost] = useState(userPost);
+  const defaultDp = "/src/assets/Images/User i/user.png";
+  const [addComment, setAddComment] = useState("");
 
   const getUserDetails = useCallback(async () => {
     const user = await getUserById(currentUser.uid);
@@ -29,27 +34,18 @@ export const ViewPostModal = ({ post }) => {
       await getUserDetails();
     };
 
-    const getDp = async (userId) => {
-      await getProfilePhotoByID(userId);
-    };
-
     if (!user) fetchData();
 
-    if (user) getDp(user.uid);
+    // if (user) getDp(user.uid);
   }, [getUserDetails, user]);
 
-  // console.log("CLickedpost", user);
-
   // Post Comment
-  const [addComment, setAddComment] = useState("");
-
   const handlePostComment = (e) => {
     const comment = e.target.value;
     setAddComment(comment);
   };
 
   // Emojis
-
   const handleEmojiDrawer = (e) => {
     setOpenEmojiDrawer(!openEmojiDrawer);
 
@@ -62,10 +58,6 @@ export const ViewPostModal = ({ post }) => {
     // SEtting emoji directly!
     setAddComment((prevComment) => prevComment + emoji);
   };
-
-  // useEffect(() => {
-  //   console.log("comment :", addComment);
-  // }, [addComment]);
 
   const emojiRef = useRef();
 
@@ -104,16 +96,63 @@ export const ViewPostModal = ({ post }) => {
     };
   }, []);
 
-  const [profilePhoto, setProfilePhoto] = useState(null);
+  // LIKE POST
+  const updatePost = async (postId, newPostData, userId, username) => {
+    const updatedPost = await updateUserPostData(
+      postId,
+      newPostData,
+      userId,
+      username
+    );
 
-  const getProfilePhotoByID = async (userId) => {
-    const dp = await getProfilePhoto(userId);
-
-    console.log(dp);
-    setProfilePhoto(dp);
+    // console.log("Post update successfull:", updatedPost);
   };
 
-  console.log("pp", profilePhoto);
+  const handleLike = async (post) => {
+    const likedByArray = Array.isArray(post.likedBy) ? post.likedBy : [];
+
+    console.log(currentUser.uid, "arr ", post);
+
+    const newIsPostLiked = !likedByArray.includes(currentUser.uid);
+
+    const updatedLikedBy = newIsPostLiked
+      ? [...likedByArray, currentUser.uid]
+      : likedByArray.filter((userId) => userId !== currentUser.uid);
+
+    // console.log(updatedLikedBy);
+
+    // checking UPDATED likedBy
+    // const postLiked = updatedLikedBy.includes(currentUser.uid) ? true : false;
+
+    const newPostData = {
+      likeCount: updatedLikedBy.length,
+      isPostLiked: newIsPostLiked,
+      likedBy: updatedLikedBy,
+    };
+
+    // 1. local update,
+    // const updatedPost = posts.map((p) =>
+    //   p.postId === post.postId ? { ...p, ...newPostData } : p
+    // );
+    setPost((prevPost) => ({ ...prevPost, ...newPostData }));
+
+    // console.log("Post Data:", post, "DATA", newPostData);
+
+    // 2. Update firestore post document.
+    try {
+      await updatePost(post.postId, newPostData, post.userId, post.username);
+    } catch (error) {
+      console.log("Error updating post: ", error);
+    }
+  };
+
+  const checkIfPostLikedByCurrUser = (likedBy) => {
+    // console.log("LikedBy [] checking before rendering post", likedBy);
+    console.log("CHECK", currentUser.uid);
+    return likedBy.includes(currentUser.uid);
+  };
+
+  const postLikedByUser = checkIfPostLikedByCurrUser(post.likedBy || []);
 
   //   ------------------------------------------------------
   return (
@@ -126,7 +165,14 @@ export const ViewPostModal = ({ post }) => {
               {/* User info */}
               <div className="vpmb-headerUserDetail">
                 <div className="vpmbcs-header-profile-photo">
-                  <img src={profilePhoto && profilePhoto} alt="profile-photo" />
+                  <img
+                    src={
+                      post.userId === currentUser.uid && !dpLoading
+                        ? dpCurrUser || defaultDp
+                        : post.profilePhoto || defaultDp
+                    }
+                    alt="profile-photo"
+                  />
                 </div>
                 <div className="vpmbcs-header-profile-username">
                   <span>{user && user.username}</span>
@@ -278,7 +324,14 @@ export const ViewPostModal = ({ post }) => {
               {/* ADD COMMENT  DIV Last*/}
               <div className="vpmb-addComment mt-5">
                 <div className="vpmb-addComment-userDp">
-                  <img src={profilePhoto} alt="" />
+                  <img
+                    src={
+                      post.userId === currentUser.uid && !dpLoading
+                        ? dpCurrUser || defaultDp
+                        : post.profilePhoto || defaultDp
+                    }
+                    alt=""
+                  />
                 </div>
                 <div className="vpmb-addComment-comment">
                   <div>
@@ -350,7 +403,11 @@ export const ViewPostModal = ({ post }) => {
                 <div className="vpcs-header-profile-photo">
                   <span>
                     <img
-                      src={profilePhoto}
+                      src={
+                        post.userId === currentUser.uid && !dpLoading
+                          ? dpCurrUser || defaultDp
+                          : post.profilePhoto || defaultDp
+                      }
                       alt="profile-photo"
                       style={{ borderRadius: "50%" }}
                     />
@@ -375,7 +432,14 @@ export const ViewPostModal = ({ post }) => {
             <div className="vp-cs-postCaption">
               <div className="vpcs-postCaptionUserDetails">
                 <div className="vpcs-postCaption-profilePhoto">
-                  <img src={profilePhoto} alt="profile-photo" />
+                  <img
+                    src={
+                      post.userId === currentUser.uid && !dpLoading
+                        ? dpCurrUser || defaultDp
+                        : post.profilePhoto || defaultDp
+                    }
+                    alt="profile-photo"
+                  />
                 </div>
                 <div className="vpcs-postCaption-userName">
                   <span>{user && user.username} </span>
@@ -415,18 +479,32 @@ export const ViewPostModal = ({ post }) => {
             <div className="vp-cs-actionBtns">
               <div className="vpcs-actionBtns-left">
                 <div className="vpcs-actionBtn-like">
-                  <span id="post-likeBtn">
-                    <svg
-                      aria-label="Like"
-                      fill="currentColor"
-                      height="24"
-                      role="img"
-                      viewBox="0 0 24 24"
-                      width="24"
-                    >
-                      <title>Like</title>
-                      <path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path>
-                    </svg>
+                  <span id="post-likeBtn" onClick={() => handleLike(post)}>
+                    {postLikedByUser ? (
+                      <svg
+                        aria-label="Unlike"
+                        fill="red"
+                        height="24"
+                        role="img"
+                        viewBox="0 0 48 48"
+                        width="24"
+                      >
+                        <title>Unlike</title>
+                        <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        aria-label="Like"
+                        fill="currentColor"
+                        height="24"
+                        width="24"
+                        role="img"
+                        viewBox="0 0 24 24"
+                      >
+                        <title>Like</title>
+                        <path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path>
+                      </svg>
+                    )}
                   </span>
                 </div>
                 <div className="vpcs-actionBtn-comment ms-3">
@@ -512,7 +590,10 @@ export const ViewPostModal = ({ post }) => {
               <div className="vpcs-likedBy-likes  mt-2">
                 <span className="ms-3">
                   {/* last three people profile photos who liked the image */}
-                  <b>109 likes</b>
+                  <b>
+                    {post.likeCount > 0 &&
+                      post.likeCount + ` like${post.likeCount > 1 ? `s` : ""}`}
+                  </b>
                 </span>
               </div>
               <div className="vpcs-likedBy-postTimeStamp"></div>
@@ -521,7 +602,14 @@ export const ViewPostModal = ({ post }) => {
             {/* Add comment */}
             <div className="vp-cs-addComment mt-3">
               <div className="vpcs-addComment-userDp">
-                <img src={profilePhoto} alt="profile-photo" />
+                <img
+                  src={
+                    post.userId === currentUser.uid && !dpLoading
+                      ? dpCurrUser || defaultDp
+                      : post.profilePhoto || defaultDp
+                  }
+                  alt="profile-photo"
+                />
               </div>
               <div className="vpcs-addComment-comment">
                 <textarea
